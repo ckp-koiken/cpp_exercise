76fa99cb90b537391e773e0435b5d97279af253f
ckp-koiken <kentaro.koi0710@gmail.com>
2025-03-13 19:49:20 +0900

    add ex3

--- ch1/ch1_main.cpp
+++ ch1/ch1_main.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 
 int main(void) {
-  cout << 2525 << endl;
+  cout << 1 + 1 << endl;
+  cout << 3 - 4 << endl;
+  cout << 2 * 3 << endl;
+  cout << 7 / 3 << endl;
+  cout << 7.0 / 3.0 << endl;
+  cout << 7 % 3 << endl;
+  cout << 4 % 5 << endl;
+  cout << (1 + 2) * 3 << endl;
   return 0;
 }
