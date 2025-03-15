ebab4c630e6e93bd42ec08d00bea9ed6b2838407
ckp-koiken <kentaro.koi0710@gmail.com>
2025-03-15 21:34:55 +0900

    add ch6

--- ch1/ch1_main.cpp
+++ ch1/ch1_main.cpp
@@ -2,8 +2,18 @@
 using namespace std;
 
 int main(void) {
-  int a, b, c;
-  cin >> a >> b >> c;
-  cout << a * b * c << endl;
+  int x;
+  cin >> x; 
+
+  if (x < 10) {
+    cout << "xは10より小さい" << endl;
+  } else if (x > 20) {
+    cout << "xは10より小さくなくて、20より大きい" << endl;
+  } else if (x == 15) {
+    cout << "xは10より小さくなくて、20より大きくなくて、15である" << endl;
+  } else {
+    cout << "xは10より小さくなくて、20より大きくなくて、15でもない" << endl;
+  }
+
   return 0;
 }
