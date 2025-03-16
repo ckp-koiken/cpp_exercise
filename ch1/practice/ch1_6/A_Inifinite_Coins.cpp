3c75ca1506ab5b5c1ccb69eeb1cebbc17abe205f
ckp-koiken <kentaro.koi0710@gmail.com>
2025-03-16 18:15:58 +0900

    add A_prob

---
+++ ch1/practice/ch1_6/A_Inifinite_Coins.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N, A;
+  cin >> N >> A;
+  int i = N % 500;
+  if (i <= A)
+    cout << "Yes" << endl;
+  else 
+    cout << "No" << endl;
+}
