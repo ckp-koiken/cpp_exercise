ba674d45fadfeba3cee5ca81021932522d220e2c
ckp-koiken <kentaro.koi0710@gmail.com>
2025-03-20 19:38:12 +0900

    fix indent

--- ch1/practice/ch1_12/A_AtCoder___Contest.cpp
+++ ch1/practice/ch1_12/A_AtCoder___Contest.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-   string A, s, C;
-   cin >> A >> s >> C; 
-   cout << A.at(0) << s.at(0) << C.at(0) << endl;
+int main() { 
+    string A, s, C; 
+    cin >> A >> s >> C; 
+    cout << A.at(0) << s.at(0) << C.at(0) << endl;
 }
