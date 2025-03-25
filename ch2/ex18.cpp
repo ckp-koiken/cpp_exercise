70a209fea31c3f788dae376ac725fd3c6261dff2
ckp-koiken <kentaro.koi0710@gmail.com>
2025-03-25 21:51:16 +0900

    add memo

--- ch2/ex18.cpp
+++ ch2/ex18.cpp
@@ -10,19 +10,28 @@ int main() {
         cin >> A.at(i) >> B.at(i);
     }
 
+    // N*Mのchar型の2次元配列の全てを'-'で初期化
     vector<vector<char>> data(N, vector<char>(N, '-'));
+
     for (int i = 0; i < M; i++) {
-        data.at(A.at(i)-1).at(B.at(i)-1) = 'o';
-        data.at(B.at(i)-1).at(A.at(i)-1) = 'x';
+        data.at(A.at(i)-1).at(B.at(i)-1) = 'o';  // AはBに勝った
+        data.at(B.at(i)-1).at(A.at(i)-1) = 'x';  // BはAに勝った
     }
+    
+    // MEMO: 最初から添字のために1減らしておくのもあり
+    // for (int i = 0; i < M; i++) {
+    //     A.at(i)--; B.at(i)--;
+    //     data.at(A.at(i)).at(B.at(i)) = 'o';
+    //     data.at(B.at(i)).at(A.at(i)) = 'x';
+    // }
 
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             cout << data.at(i).at(j);
-            if (j == N-1) {
-                cout << endl;
+            if (j == N - 1) {
+                cout << endl;  // 行末なら改行
             } else {
-                cout << " ";
+                cout << " ";   // 行末でないなら空白を出力
             }
         }
     }
