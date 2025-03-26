1a0f6ecabd5830efd35281708ed9d91b3a2fb3c9
ckp-koiken <kentaro.koi0710@gmail.com>
2025-03-26 23:14:23 +0900

    add ex19

--- ch2/ch2_main.cpp
+++ ch2/ch2_main.cpp
@@ -1,31 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int sum100(vector<int> &a) {
+    int result = 0;
+    for (int i = 0; i < 100; i++) {
+        result += a.at(i);
+    }
+    return result;
+}
+
 int main() {
-    int N;
-    cin >> N;
+    vector<int> vec(10000000, 1);
 
-    vector<vector<vector<char>>> data(N, vector<vector<char>>(3, vector<char>(3)));
-
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            for (int k = 0; k < N; k++) {
-                cin >> data.at(i).at(j).at(k);
-            }
-        }
+    for (int i = 0; i < 500; i++) {
+        cout << sum100(vec) << endl;
     }
-
-    int count = 0;
-
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            for (int k = 0; k < N; k++) {
-                if (data.at(i).at(j).at(k) == 'o') {
-                    count++;
-                }
-            }
-        }
-    }
-
-    cout << count << endl;
 }
