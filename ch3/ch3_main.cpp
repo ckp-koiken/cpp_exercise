25c6596c3ec3c437c125c24099a06b0954a6bfdd
ckp-koiken <kentaro.koi0710@gmail.com>
2025-04-01 17:27:27 +0900

    add 3.01

---
+++ ch3/ch3_main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    vector<int> data(0);
+    cout << data.size() - 1 << endl;
+
+    for (int i = 0; i < (int)data.size() - 1; i++) {
+        cout << i << endl;
+    }
+}
---
+++ ch3/practice/B_Lucas_Number.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    vector<int64_t> L(87);
+    L.at(0) = 2LL;
+    L.at(1) = 1LL;
+    
+    for (int i = 2; i <= N; i++) {
+        L.at(i) = L.at(i - 2) + L.at(i - 1);
+    }
+
+    cout << L.at(N) << endl;
+}
