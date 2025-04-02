9757f07ba08a5e272cfe8b1a442380d9f6e2ae1f
ckp-koiken <kentaro.koi0710@gmail.com>
2025-04-02 12:12:32 +0900

    add answer

--- ch3/ex22.cpp
+++ ch3/ex22.cpp
@@ -5,19 +5,29 @@ int main() {
     int N, a, b;
     cin >> N;
 
-    pair<int, int> p(0, 0);
     vector<pair<int, int>> vec(N);
 
     for (int i = 0; i < N; i++) {
         cin >> vec.at(i).second >> vec.at(i).first;
     }
+
+    // 解答例
+    // for (int i = 0; i < N; i++) {
+    //     int a, b;
+    //     cin >> a >> b;
+    //     vec.at(i) = make_pair(b, a);
+    // }
+
     sort(vec.begin(), vec.end());
 
-    for (pair<int, int> &t : vec) {
-        int tmp;
-        tmp = t.second;
-        t.second = t.first;
-        t.first = tmp;
-        cout << t.first << " " << t.second << endl;
+    for (pair<int, int> t : vec) {
+        cout << t.second << " " << t.first << endl;
     }
+
+    // 解答例
+    // for (int i = 0; i < N; i++) {
+    //     int b, a;
+    //     tie(b, a) = vec.at(i);
+    //     cout << a << " " << b << endl;
+    // }
 }
