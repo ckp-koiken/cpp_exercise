42a46fa307f6eaf45586503bcd9bc2e060151ee2
ckp-koiken <kentaro.koi0710@gmail.com>
2025-04-03 16:00:53 +0900

    add ex23

--- ch3/ch3_main.cpp
+++ ch3/ch3_main.cpp
@@ -1,14 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 int main() {
-    // using pii = pair<int, int>;
-    typedef pair<int, int> pii;
-
-    pii p;
-    p = make_pair(1, 2);
-    cout << "(" << p. first << ", " << p.second << ")" << endl;
-    p = pii(3, 4);
-    cout << "(" << p. first << ", " << p.second << ")" << endl;
+  queue<int> q;
+  cout << q.front() << endl;
 }
