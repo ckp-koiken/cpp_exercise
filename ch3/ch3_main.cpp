77d8e0ecd5207176d57a188898cae2b01caf4677
ckp-koiken <kentaro.koi0710@gmail.com>
2025-04-05 18:48:54 +0900

    add 3.04

--- ch3/ch3_main.cpp
+++ ch3/ch3_main.cpp
@@ -5,19 +5,12 @@ struct MyPair {
   int x;
   string y;
 
-  void operator=(const MyPair &other) {
-    cout << "= operator called" << endl;
-    x = other.x;
-    y = other.y;
+  MyPair() : x(123), y("hello") {
   }
 };
 
 int main() {
-  MyPair a = {123, "hello"};
-  MyPair b;
-
-  b = a;
-
-  cout << "b.x = " << b.x << endl;
-  cout << "b.y = " << b.y << endl;
+  MyPair p;
+  cout << "p.x = " << p.x << endl;
+  cout << "p.y = " << p.y << endl;
 }
