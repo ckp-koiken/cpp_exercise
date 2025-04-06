e3eed1f2cce1bc4cf70bce67fcf4a89bc2bb8187
ckp-koiken <kentaro.koi0710@gmail.com>
2025-04-06 18:25:21 +0900

    add clock & set

---
+++ ch3/ex24.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Clock {
+  int hour;
+  int minute;
+  int second;
+
+  void set(int h, int m, int s) {
+    hour = h;
+    minute = m;
+    second = s;
+  }
+
+  string to_str(void) {
+    string ret;
+
+    if (hour < 10) {
+      ret += 10;
+    }
+    ret += to_string(hour);
+
+    ret += ":";
+
+    if (minute < 10) {
+      ret += "0";
+    }
+    ret += to_string(minute);
+
+    ret += ":";
+
+    if (second < 10) {
+      ret += "0";
+    }
+    ret += to_string(second);
+
+    return ret;
+  }
+
+  // void shift(int diff_second) {
+  //   if (diff_second < 0) {
+  //     second -= diff_second;
+  //   } else {
+  //     second += diff_second;
+  //   }
+  // }
+
+};
+
+
+int main() {
+  int hour, minute, second;
+  cin >> hour >> minute >> second;
+  int diff_second;
+  cin >> diff_second;
+
+  Clock clock;
+
+  clock.set(hour, minute, second);
+
+  cout << clock.to_str() << endl;
+
+  // clock.shift(diff_second);
+
+  // cout << clock.to_str() << endl;
+
+}
